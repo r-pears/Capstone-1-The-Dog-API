d72d8eb2b0d5cdc7bfc77c40f51b8de99e63c33f
Ryjen <ryjen.pears@gmail.com>
2022-01-16 00:14:14 +0100

    added new line

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my_secret_key'
 app.config['SQLALCHEMY_DATABASE_URL'] = 'postgresql:///dog-app'
 
+
 @app.route('/')
 def homepage():
     """Show the index page."""

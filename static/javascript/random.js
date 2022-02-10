f92be9e96e97b41cc8c0a9cd1794d055bcedb7f9
Ryjen <ryjen.pears@gmail.com>
2022-02-10 16:50:48 +0100

    class update

--- static/javascript/random.js
+++ static/javascript/random.js
@@ -6,13 +6,18 @@ async function fetchRandomDog() {
 
   result.innerHTML = '';
 
-  let newPara = document.createElement('p');
+  let newPara = document.createElement('div');
   let newDog = document.createElement('a');
-    newDog.innerHTML = `Read more about ` + randomDog.name + ` here.`;
+    newDog.innerHTML = `Learn more about ` + randomDog.name + ` here.`;
     newDog.setAttribute('href', `/breed/${randomDog.name}`);
   const newImage = document.createElement('img');
     newImage.setAttribute('src', randomDog.image.url);
     newImage.classList.add('dog-image');
+    newImage.classList.add('top_margin');
+  
+
+  newPara.classList.add('flex-col');
+  newPara.classList.add('top_margin');
 
   newPara.appendChild(newDog);
   newPara.appendChild(newImage);
